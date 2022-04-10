6d5333d3a4b1acea749404b1d85e56d46aed2c8d
FrancoFantomius <56490563+FrancoFantomius@users.noreply.github.com>
2022-04-10 19:14:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e =>{
     e.waitUntil(
             caches.open("static").then(cache => {
-                return cache.addAll(["./index.html", "./src/index.css", "./img/logo-192.png"]);
+                return cache.addAll(["./", "/src/index.css", "/img/logo-192.png"]);
             }
         )
     )
